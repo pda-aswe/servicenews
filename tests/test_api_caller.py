806e7251a6b2b00e0cc661b8c75797564b493a26
RBenzin <83594506+RBenzin@users.noreply.github.com>
2023-04-18 23:55:35 +0200

    updated tests, now 100% coverage

--- tests/test_api_caller.py
+++ tests/test_api_caller.py
@@ -3,6 +3,18 @@ import pytest
 from unittest.mock import Mock, patch
 from src.api_caller import WorldNewsApiCaller
 
+def test_init_no_key_file(capfd, monkeypatch):
+    def mock_quit():
+        pass
+
+    monkeypatch.setattr(os.path, "exists", lambda x: False)
+    monkeypatch.setattr("builtins.quit", mock_quit)
+
+    WorldNewsApiCaller()
+
+    captured = capfd.readouterr()
+    assert "api key file missing" in captured.out
+
 # Create a temporary key file for testing
 @pytest.fixture(scope="module")
 def create_key_file():
@@ -12,9 +24,15 @@ def create_key_file():
     yield key_file
     os.remove(key_file)
 
-def test_load_api_key(create_key_file):
+def test_load_api_key(create_key_file, monkeypatch):
+    def mock_init(self):
+        self.baseURL = "https://api.worldnewsapi.com/search-news?"
+        self.apiKey = None
+
+    monkeypatch.setattr(WorldNewsApiCaller, "__init__", mock_init)
     api_caller = WorldNewsApiCaller()
-    assert api_caller.apiKey == "test_key"
+    api_key = api_caller._WorldNewsApiCaller__loadAPIKey()
+    assert api_key == "test_key"
 
 def test_news_request_invalid_number_of_articles():
     api_caller = WorldNewsApiCaller()
@@ -28,7 +46,7 @@ def test_news_request_invalid_location():
     assert api_caller.newsRequest(1, {"latitude": 0}) == -1
     assert api_caller.newsRequest(1, {"lat": 0, "lon": 0}) == -1
 
-@patch("api_caller.requests.post")
+@patch("api_caller.requests.get")
 def test_news_request_success(mock_post):
     api_caller = WorldNewsApiCaller()
     response = Mock()
@@ -40,3 +58,14 @@ def test_news_request_success(mock_post):
     assert len(articles) == 1
     assert articles[0]["summary"] == "Test summary"
     assert articles[0]["text"] == "Test text"
+
+@patch("api_caller.requests.get")
+def test_news_request_invalid_json(mock_get):
+    api_caller = WorldNewsApiCaller()
+    response = Mock()
+    response.status_code = 200
+    response.text = '{"news": [{"summary": "Test summary", "text": "Test text"}'  # Missing closing brace
+    mock_get.return_value = response
+
+    articles = api_caller.newsRequest(1)
+    assert articles == -1
